70e46da944d88eabe3502ff8e608da0cb3be55f7
vishnulinga <vishnulinga2@gmail.com>
2020-09-27 13:41:04 +0530

    build

---
+++ build/precache-manifest.476b8f3e40bdb4eb33ad24ececebc7e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e745fb227412343f070c151738209388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eb2188f7687a5e10b68",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "080ff46538ba5fe98843",
+    "url": "/static/js/2.7faf3393.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7faf3393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb2188f7687a5e10b68",
+    "url": "/static/js/main.eae992a6.chunk.js"
+  },
+  {
+    "revision": "a08661e7d787038fc70d",
+    "url": "/static/js/runtime-main.fa76c016.js"
+  }
+]);
